5b1844086c9d9a2f385595a51e9fc2f655e689cc
Balqis Mughny Sasando <69091426+BLQ5@users.noreply.github.com>
2020-08-31 01:15:45 +0700

    Initialize track_widget_creation_checkbox

---
+++ src/io/flutter/actions/OpenInAndroidStudioAction.java
@@ -0,0 +1,224 @@
+/*
+ * Copyright 2017 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+package io.flutter.actions;
+
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.execution.process.OSProcessHandler;
+import com.intellij.execution.process.ProcessAdapter;
+import com.intellij.execution.process.ProcessEvent;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.vfs.VirtualFile;
+import io.flutter.FlutterBundle;
+import io.flutter.FlutterMessages;
+import io.flutter.pub.PubRoot;
+import io.flutter.pub.PubRoots;
+import io.flutter.sdk.FlutterSdk;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+
+public class OpenInAndroidStudioAction extends AnAction {
+
+  private static void openFileInStudio(@NotNull VirtualFile file, @NotNull String androidStudioPath) {
+    try {
+      final GeneralCommandLine cmd;
+      if (SystemInfo.isMac) {
+        cmd = new GeneralCommandLine().withExePath("open").withParameters("-a", androidStudioPath, file.getPath());
+      }
+      else {
+        cmd = new GeneralCommandLine().withExePath(androidStudioPath).withParameters(file.getPath());
+      }
+      final OSProcessHandler handler = new OSProcessHandler(cmd);
+      handler.addProcessListener(new ProcessAdapter() {
+        @Override
+        public void processTerminated(@NotNull final ProcessEvent event) {
+          if (event.getExitCode() != 0) {
+            FlutterMessages.showError("Error Opening", file.getPath());
+          }
+        }
+      });
+      handler.startNotify();
+    }
+    catch (ExecutionException ex) {
+      FlutterMessages.showError(
+        "Error Opening",
+        "Exception: " + ex.getMessage());
+    }
+  }
+
+  protected static boolean isProjectFileName(String name) {
+    return name.endsWith("_android.iml");
+  }
+
+  @Nullable
+  private static VirtualFile findStudioProjectFile(@NotNull Project project) {
+    for (PubRoot root : PubRoots.forProject(project)) {
+      for (VirtualFile child : root.getRoot().getChildren()) {
+        if (isProjectFileName(child.getName())) {
+          return child;
+        }
+      }
+    }
+
+    return null;
+  }
+
+  @Nullable
+  private static String findAndroidStudio(@Nullable Project project) {
+    if (project == null) {
+      return null;
+    }
+
+    final FlutterSdk flutterSdk = FlutterSdk.getFlutterSdk(project);
+    if (flutterSdk != null) {
+      String androidSdkLocation = flutterSdk.queryFlutterConfig("android-studio-dir", true);
+      if (androidSdkLocation != null) {
+        if (androidSdkLocation.contains("/Android Studio 2")) {
+          Messages.showErrorDialog(FlutterBundle.message("old.android.studio.message", File.separator),
+                                   FlutterBundle.message("old.android.studio.title"));
+          return null;
+        }
+        if (androidSdkLocation.endsWith("/")) {
+          androidSdkLocation = androidSdkLocation.substring(0, androidSdkLocation.length() - 1);
+        }
+        final String contents = "/Contents";
+        // On a mac, trim off "/Contents".
+        if (SystemInfo.isMac && androidSdkLocation.endsWith(contents)) {
+          return androidSdkLocation.substring(0, androidSdkLocation.length() - contents.length());
+        }
+        return androidSdkLocation;
+      }
+    }
+    return null;
+  }
+
+  @Nullable
+  private static VirtualFile getProjectForFile(@Nullable VirtualFile file) {
+    // Expect true: isProjectFileName(file.getName()), but some flexibility is allowed.
+    if (file == null) {
+      return null;
+    }
+    if (file.isDirectory()) {
+      return isAndroidWithApp(file) ? file : null;
+    }
+    VirtualFile dir = file.getParent();
+    if (isAndroidWithApp(dir)) {
+      // In case someone moves the .iml file, or the project organization gets rationalized.
+      return dir;
+    }
+    VirtualFile project = dir.findChild("android");
+    if (project != null && isAndroidWithApp(project)) {
+      return project;
+    }
+    return null;
+  }
+
+  // Return true if the directory is named android and contains either an app (for applications) or a src (for plugins) directory.
+  private static boolean isAndroidWithApp(@NotNull VirtualFile file) {
+    return file.getName().equals("android") && (file.findChild("app") != null || file.findChild("src") != null);
+  }
+
+  // Return true if the directory has the structure of a plugin example application: a pubspec.yaml and an
+  // android directory with an app. The example app directory name is not specified in case it gets renamed.
+  private static boolean isExampleWithAndroidWithApp(@NotNull VirtualFile file) {
+    boolean hasPubspec = false;
+    boolean hasAndroid = false;
+    for (VirtualFile candidate : file.getChildren()) {
+      if (isAndroidWithApp(candidate)) hasAndroid = true;
+      if (candidate.getName().equals("pubspec.yaml")) hasPubspec = true;
+      if (hasAndroid && hasPubspec) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // A plugin contains an example app, which needs to be opened when the native Android is to be edited.
+  // In the case of an app that contains a plugin the flutter_app/flutter_plugin/example/android should be opened when
+  // 'Open in Android Studio' is requested.
+  protected static VirtualFile findProjectFile(@Nullable AnActionEvent e) {
+    if (e != null) {
+      final VirtualFile file = CommonDataKeys.VIRTUAL_FILE.getData(e.getDataContext());
+      if (file != null && file.exists()) {
+        // We have a selection. Check if it is within a plugin.
+        final Project project = e.getProject();
+        assert (project != null);
+
+        // Return null if this is an ios folder.
+        if (FlutterExternalIdeActionGroup.isWithinIOsDirectory(file, project)) {
+          return null;
+        }
+
+        final VirtualFile projectDir = project.getBaseDir();
+        for (PubRoot root : PubRoots.forProject(project)) {
+          if (root.isFlutterPlugin()) {
+            VirtualFile rootFile = root.getRoot();
+            VirtualFile aFile = file;
+            while (aFile != null) {
+              if (aFile.equals(rootFile)) {
+                // We know a plugin resource is selected. Find the example app for it.
+                for (VirtualFile child : rootFile.getChildren()) {
+                  if (isExampleWithAndroidWithApp(child)) {
+                    return child.findChild("android");
+                  }
+                }
+              }
+              if (aFile.equals(projectDir)) {
+                aFile = null;
+              }
+              else {
+                aFile = aFile.getParent();
+              }
+            }
+          }
+        }
+        if (isProjectFileName(file.getName())) {
+          return getProjectForFile(file);
+        }
+      }
+
+      final Project project = e.getProject();
+      if (project != null) {
+        return getProjectForFile(findStudioProjectFile(project));
+      }
+    }
+    return null;
+  }
+
+  @Override
+  public void update(AnActionEvent event) {
+    final boolean enabled = findProjectFile(event) != null;
+
+    final Presentation presentation = event.getPresentation();
+    presentation.setEnabled(enabled);
+    presentation.setVisible(enabled);
+  }
+
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+    final String androidStudioPath = findAndroidStudio(e.getProject());
+    if (androidStudioPath == null) {
+      FlutterMessages.showError("Error Opening Android Studio", "Unable to locate Android Studio.");
+      return;
+    }
+
+    final VirtualFile projectFile = findProjectFile(e);
+    if (projectFile == null) {
+      FlutterMessages.showError("Error Opening Android Studio", "Project not found.");
+      return;
+    }
+
+    openFileInStudio(projectFile, androidStudioPath);
+  }
+}
---
+++ src/io/flutter/actions/RunFlutterAction.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2017 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+package io.flutter.actions;
+
+import com.intellij.execution.Executor;
+import com.intellij.execution.ProgramRunnerUtil;
+import com.intellij.execution.RunManagerEx;
+import com.intellij.execution.RunnerAndConfigurationSettings;
+import com.intellij.execution.configurations.RunConfiguration;
+import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.execution.runners.ExecutionEnvironmentBuilder;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import io.flutter.FlutterBundle;
+import io.flutter.FlutterInitializer;
+import io.flutter.run.FlutterLaunchMode;
+import io.flutter.run.LaunchState;
+import io.flutter.run.SdkFields;
+import io.flutter.run.SdkRunConfig;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public abstract class RunFlutterAction extends AnAction {
+  private final @NotNull String myDetailedTextKey;
+  private final @NotNull FlutterLaunchMode myLaunchMode;
+  private final @NotNull String myExecutorId;
+
+  public RunFlutterAction(@NotNull String text,
+                          @NotNull String detailedTextKey,
+                          @NotNull String description,
+                          @NotNull Icon icon,
+                          @NotNull FlutterLaunchMode launchMode,
+                          @NotNull String executorId) {
+    super(text, description, icon);
+
+    myDetailedTextKey = detailedTextKey;
+    myLaunchMode = launchMode;
+    myExecutorId = executorId;
+  }
+
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+    FlutterInitializer.sendAnalyticsAction(this);
+
+    final RunnerAndConfigurationSettings settings = getRunConfigSettings(e);
+    if (settings == null) {
+      return;
+    }
+
+    final RunConfiguration configuration = settings.getConfiguration();
+    if (!(configuration instanceof SdkRunConfig)) {
+      // Action is disabled; shouldn't happen.
+      return;
+    }
+
+    final SdkRunConfig sdkRunConfig = (SdkRunConfig)configuration.clone();
+    final SdkFields fields = sdkRunConfig.getFields();
+    final String additionalArgs = fields.getAdditionalArgs();
+    final String runArg = "--" + myLaunchMode.getCliCommand();
+    String flavorArg = "";
+    if (fields.getBuildFlavor() != null) {
+      flavorArg = " --flavor=" + fields.getBuildFlavor();
+    }
+
+    if (additionalArgs == null) {
+      fields.setAdditionalArgs(runArg + flavorArg);
+    }
+    else {
+      if (!additionalArgs.contains(runArg)) {
+        fields.setAdditionalArgs(additionalArgs + flavorArg + " " + runArg);
+      }
+    }
+
+    final Executor executor = getExecutor(myExecutorId);
+    if (executor == null) {
+      return;
+    }
+
+    final ExecutionEnvironmentBuilder builder = ExecutionEnvironmentBuilder.create(executor, sdkRunConfig);
+    final ExecutionEnvironment env = builder.activeTarget().dataContext(e.getDataContext()).build();
+
+    env.putUserData(FlutterLaunchMode.LAUNCH_MODE_KEY, myLaunchMode);
+
+    ProgramRunnerUtil.executeConfiguration(env, false, true);
+  }
+
+  @Override
+  public void update(AnActionEvent e) {
+    // Text.
+    final String config = getSelectedRunConfig(e);
+    final String message =
+      config != null ? FlutterBundle.message(myDetailedTextKey, config) : FlutterBundle.message("app.profile.action.text");
+    e.getPresentation().setText(message);
+
+    // Enablement.
+    e.getPresentation().setEnabled(shouldEnable(e));
+  }
+
+  private static boolean shouldEnable(@Nullable AnActionEvent e) {
+    final RunnerAndConfigurationSettings settings = getRunConfigSettings(e);
+    final RunConfiguration config = settings == null ? null : settings.getConfiguration();
+    // TODO(pq): add support for Bazel.
+    return config instanceof SdkRunConfig && LaunchState.getRunningAppProcess((SdkRunConfig)config) == null;
+  }
+
+  @Nullable
+  protected static String getSelectedRunConfig(@Nullable AnActionEvent e) {
+    final RunnerAndConfigurationSettings settings = getRunConfigSettings(e);
+    if (settings != null) {
+      final RunConfiguration configuration = settings.getConfiguration();
+      if (configuration != null) {
+        return configuration.getName();
+      }
+    }
+    return null;
+  }
+
+  @Nullable
+  private static RunnerAndConfigurationSettings getRunConfigSettings(@Nullable AnActionEvent event) {
+    if (event == null) {
+      return null;
+    }
+
+    final Project project = event.getProject();
+    if (project == null) {
+      return null;
+    }
+
+    return RunManagerEx.getInstanceEx(project).getSelectedConfiguration();
+  }
+
+  @Nullable
+  private static Executor getExecutor(@NotNull String executorId) {
+    for (Executor executor : Executor.EXECUTOR_EXTENSION_NAME.getExtensions()) {
+      if (executorId.equals(executor.getId())) {
+        return executor;
+      }
+    }
+
+    return null;
+  }
+}
